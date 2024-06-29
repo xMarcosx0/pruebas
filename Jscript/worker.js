83b3abe2f7a99847b98a337312835420b9c206bf
xMarcosx0 <158768106+xMarcosx0@users.noreply.github.com>
2024-06-29 09:33:06 -0500

    Add files via upload

---
+++ Jscript/worker.js
@@ -0,0 +1,15 @@
+function mostrarSeccion(seccionId) {
+    const secciones = document.querySelectorAll('main section');
+    secciones.forEach(seccion => {
+        if (seccion.id === seccionId) {
+            seccion.classList.remove('hidden');
+        } else {
+            seccion.classList.add('hidden');
+        }
+    });
+}
+
+function cerrarSesion() {
+    localStorage.clear();
+    window.location.href = "../Html/login.html";
+}
